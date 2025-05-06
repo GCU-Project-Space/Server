beb2b5a3ab78e7d54e0802fa6ae68b6b6ac2a416
eedo_y <doup2001@naver.com>
2025-05-06 13:54:02 +0900

    ✨ feat : 공통 응답 객체 생성

---
+++ payment-service/src/main/java/gcu/web/paymentservice/common/response/CustomException.java
@@ -0,0 +1,14 @@
+package gcu.web.paymentservice.common.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public String getMessage() {
+        return errorCode.getMessage();
+    }
+}
