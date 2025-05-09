0d0fbe046d43a52589cfc2ecb70ac6782e3f18b9
wldsmdl7 <wldmsdl5835@gmail.com>
2025-05-09 16:21:40 +0900

    feat: 회원가입 구현

--- user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -1,5 +1,22 @@
 package com.example.user_service.controller;
 
+import org.springframework.web.bind.annotation.*;  
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import com.example.user_service.DTO.UserRequestDTO;
+import com.example.user_service.DTO.UserResponseDTO;
+import com.example.user_service.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users/signup")
+@RequiredArgsConstructor
 public class UserController {
-    
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> signup(@RequestBody UserRequestDTO requestDTO) {
+        UserResponseDTO responseDTO = userService.createUser(requestDTO);
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
 }
--- user-service/src/main/java/com/example/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/example/user_service/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.example.user_service.repository;
 
-public class UserRepository {
-    
+import com.example.user_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailEndingWith(String domain);
 }
