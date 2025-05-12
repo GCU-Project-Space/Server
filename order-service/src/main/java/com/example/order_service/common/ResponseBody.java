e81b8e96c6902266ef699a5729cc826cb46ca9e2
kmj02dev <kmj02dev@gmail.com>
2025-05-12 23:07:36 +0900

    refactor : ResponseBody 리팩토링
    fail 함수 일반화, success 함수 추가, update 함수 data 인자 삭제,  함수 정렬

--- order-service/src/main/java/com/example/order_service/common/ResponseBody.java
+++ order-service/src/main/java/com/example/order_service/common/ResponseBody.java
@@ -10,26 +10,28 @@ public record ResponseBody<T>(
 
 ) {
 
-    public static <T> ResponseBody<T> ok(@Nullable final T data, @Nullable final String message) {
-        return new ResponseBody<>(true, 1000, message, data);
+    public static <T> ResponseBody<T> success(@Nullable final T data, final int code,  @Nullable final String message) {
+        return new ResponseBody<>(true, code, message, data);
+    }
+
+    public static <T> ResponseBody<T> fail(@Nullable final T data, final int code,  @Nullable final String message) {
+        return new ResponseBody<>(false, code, message, data);
     }
 
     public static <T> ResponseBody<T> created(@Nullable final T data, @Nullable final String message) {
         return new ResponseBody<>(true, 1001, message, data);
     }
 
-    public static <T> ResponseBody<T> updated(@Nullable final T data, @Nullable final String message) {
-        return new ResponseBody<>(true, 1003, message, data);
+    public static <T> ResponseBody<T> ok(@Nullable final T data, @Nullable final String message) {
+        return new ResponseBody<>(true, 1000, message, data);
+    }
+
+    public static <T> ResponseBody<T> updated(@Nullable final String message) {
+        return new ResponseBody<>(true, 1003, message, null);
     }
 
     public static <T> ResponseBody<T> deleted(@Nullable final String message) {
         return new ResponseBody<>(true, 1003, message, null);
     }
-
-    public static <T> ResponseBody<T> fail(final CustomException e) {
-        ExceptionDto dto = ExceptionDto.of(e.getErrorCode());
-        return new ResponseBody<>(false, dto.getCode(), dto.getMessage(), null);
-    }
-
 
 }
