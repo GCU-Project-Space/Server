0e4d6de638ececc506a9dd1eb607a70ad11e75ab
kmj02dev <kmj02dev@gmail.com>
2025-05-13 00:12:56 +0900

    feat : ExceptionHandler 추가
    CustomException getter 수정, GlobalExceptionHandler 추가

--- order-service/src/main/java/com/example/order_service/common/CustomException.java
+++ order-service/src/main/java/com/example/order_service/common/CustomException.java
@@ -1,14 +1,22 @@
 package com.example.order_service.common;
 
-import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @RequiredArgsConstructor
 public class CustomException extends RuntimeException{
     private final ErrorCode errorCode;
 
     public String getMessage() {
         return errorCode.getMessage();
+    }
+
+    public HttpStatus getHttpStatus() {
+        return errorCode.getHttpStatus();
+    }
+
+    public int getCode() {
+        return errorCode.getCode();
     }
 }
