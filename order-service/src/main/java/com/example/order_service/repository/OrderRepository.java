0426a0f2232782e6e48e421a5ed551b14567c12d
kmj02dev <kmj02dev@gmail.com>
2025-05-13 00:32:46 +0900

    fix : 주문들을 조회하는 연산에서 없는 id를 조회했음에도 에러가 발생하지 않는 문제 해결
    원인 : JPA에서 List<T>가 반환형인 인터페이스는 레코드를 찾지 못해도 null이 아닌 빈 리스트가 반환되어 orElseThrow가 의미가 없음.

--- order-service/src/main/java/com/example/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/example/order_service/repository/OrderRepository.java
@@ -1,11 +1,8 @@
 package com.example.order_service.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.order_service.entity.OrderEntity;
@@ -13,13 +10,10 @@ import com.example.order_service.entity.OrderEntity;
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
-    @Query("SELECT o FROM OrderEntity o WHERE userId = :userId")
-    Optional<List<OrderEntity>> findByUserId(@Param("userId") Long userId);
+    List<OrderEntity> findByUserId(Long userId); // 반환형이 List<T>라면 JPA는 null이 아닌 빈 리스트를 반환한다.
 
-    @Query("SELECT o FROM OrderEntity o WHERE groupId = :groupId")
-    Optional<List<OrderEntity>> findByGroupId(@Param("groupId") Long groupId);
+    List<OrderEntity> findByGroupId(Long groupId);
 
-    @Query("SELECT o FROM OrderEntity o WHERE storeId = :storeId")
-    Optional<List<OrderEntity>> findByStoreId(@Param("storeId") Long storeId);
+    List<OrderEntity> findByStoreId(Long storeId);
     
 }
--- order-service/src/main/java/com/example/order_service/service/OrderService.java
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -63,15 +63,15 @@ public class OrderService{
     }
 
     public List<OrderResponse> getOrdersByUserId(Long userId) {
-        List<OrderEntity> orderEntities = orderRepository.findByUserId(userId)
-        .orElseThrow(() -> new RuntimeException("User not found"));
+        List<OrderEntity> orderEntities = orderRepository.findByUserId(userId);
+        if (orderEntities.isEmpty()) throw new RuntimeException("User not found");
 
         return orderEntities.stream().map(order -> order.toResponse()).collect(Collectors.toList());
     }
 
     public List<OrderResponse> getOrdersByGroupId(Long groupId) {
-        List<OrderEntity> orderEntities = orderRepository.findByGroupId(groupId)
-        .orElseThrow(() -> new RuntimeException("Group not found"));
+        List<OrderEntity> orderEntities = orderRepository.findByGroupId(groupId);
+        if (orderEntities.isEmpty()) throw new RuntimeException("Group not found");
 
         return orderEntities.stream().map(order -> order.toResponse()).collect(Collectors.toList());
     }
@@ -120,8 +120,8 @@ public class OrderService{
     }
 
     public void cancelOrdersByGroupId(Long groupId) {
-        List<OrderEntity> orderEntities = orderRepository.findByGroupId(groupId)
-        .orElseThrow(() -> new RuntimeException("Group not found"));
+        List<OrderEntity> orderEntities = orderRepository.findByGroupId(groupId);
+        if (orderEntities.isEmpty()) throw new RuntimeException("Group not found");
 
         orderRepository.saveAll(orderEntities.stream()
             .map(order -> {
@@ -132,15 +132,18 @@ public class OrderService{
     }
 
     public List<OrderResponse> getOrdersByStoreId(Long storeId) {
-        return orderRepository.findByStoreId(storeId)
-        .orElseThrow(() -> new RuntimeException("Store not found"))
-        .stream().map(order -> order.toResponse()).collect(Collectors.toList());
+
+        List<OrderEntity> orderEntities = orderRepository.findByStoreId(storeId);
+        if (orderEntities.isEmpty()) throw new RuntimeException("Group not found");
+
+        return orderEntities.stream().map(order -> order.toResponse()).collect(Collectors.toList());
     }
 
     public List<OrderResponse> getCurrentOrdersByStoreId(Long storeId) {
-        return orderRepository.findByStoreId(storeId)
-        .orElseThrow(() -> new RuntimeException("Store not found"))
-        .stream().filter(order -> order.getStatus() == OrderStatus.PAID).map(order -> order.toResponse()).collect(Collectors.toList());
+        List<OrderEntity> orderEntities = orderRepository.findByStoreId(storeId);
+        if (orderEntities.isEmpty()) throw new RuntimeException("Group not found");
+        
+        return orderEntities.stream().filter(order -> order.getStatus() == OrderStatus.PAID).map(order -> order.toResponse()).collect(Collectors.toList());
     }
 
 }
