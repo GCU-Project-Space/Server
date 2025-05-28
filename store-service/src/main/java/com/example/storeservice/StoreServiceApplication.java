5670fc0e7a21103d95bc222dabe3adce73448bea
eedo_y <doup2001@naver.com>
2025-05-28 18:31:28 +0900

    ✨ feat : 각 서비스 내 게이트웨이 설정 추가 - gradle 및 yml 설정

--- store-service/src/main/java/com/example/storeservice/StoreServiceApplication.java
+++ store-service/src/main/java/com/example/storeservice/StoreServiceApplication.java
@@ -2,11 +2,12 @@ package com.example.storeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients 
-
+@EnableFeignClients
+@EnableDiscoveryClient
 public class StoreServiceApplication {
 
 	public static void main(String[] args) {
