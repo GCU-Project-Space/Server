82a1c4c242a637272d0b930bd8e0879cd2113d8f
eedo_y <doup2001@naver.com>
2025-06-02 01:48:21 +0900

    ♻️ refactor : 결젯 서비스 CORS 주소 수정

--- payment-service/src/main/java/gcu/web/paymentservice/common/config/WebMvcConfig.java
+++ payment-service/src/main/java/gcu/web/paymentservice/common/config/WebMvcConfig.java
@@ -10,10 +10,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://54.66.149.225", "http://34.127.7.212")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")  // 모든 헤더 허용
-                .allowCredentials(true);  // 쿠키 포함 허용;
+                .allowedHeaders("*");  // 모든 헤더 허용
+        //.allowCredentials(true);  // 쿠키 포함 허용;
 
         // addMapping : CORS를 적용할 url의 패턴을 정의 ("/**"로 모든 패턴 가능)
         // allowedOrigins : 허용할 origin ("*"로 모든 origin 허용 가능, 여러 개도 지정 가능)
@@ -21,4 +21,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // maxAge : 요청을 캐싱할 시간
     }
 }
+
 
